c57657078205f0d61e87335df766910149077ca2
Gerson-Santiago <gersondossantossantiago@gmail.com>
2025-07-31 07:33:37 -0300

    Desafio organizar baralho

---
+++ uso.js
@@ -0,0 +1,8 @@
+// baralho/uso.js
+import ordenarDeck from "./util.js";
+
+// Exemplo de entrada
+const deck = ["Q♠", "3♦", "A♣", "10♥", "2♣", "K♦"];
+
+// Exemplo de entrada
+console.log(ordenarDeck(deck))
---
+++ util.js
@@ -0,0 +1,20 @@
+// baralho/teste.js
+import getNaipes from "./config_baralho/naipes.js";
+import getCartas from "./config_baralho/cartas.js";
+
+const valores = getCartas();
+const naipes = getNaipes();
+
+export default function ordenarDeck(deck) {
+    deck.forEach(element => {
+        let carta = element[0]
+        let naipe = element[1]
+        console.log(`carta ${carta}`)        
+        console.log(`naipe ${naipe}`)
+
+        
+    });
+
+
+    return deck;
+}
