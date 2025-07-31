6668b31f846cfe5e4d5ea15d2d346396712019ba
Gerson Santiago <gersondossantossantiago@gmail.com>
2025-07-31 13:25:37 -0300

    Ajustes no setDic

--- util.js
+++ util.js
@@ -1,14 +1,14 @@
 // baralho/util.js
 import getNaipes from "./config_baralho/naipes.js";
 import getCartas from "./config_baralho/cartas.js";
-import setDict from "./config_baralho/dic.js";
+import setDic from "./config_baralho/setDic.js";
 
 const valores = getCartas(); // ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 const naipes = getNaipes(); // ["♣", "♦", "♥", "♠"]
 
 export default function ordenarDeck(deck) {
     // Mapear
-    let mapasCartas = setDict(deck);
+    let mapasCartas = setDic(deck);
     valores.forEach(element => {
         console.log(`Ordenando naipe: ${element}`);
     });
