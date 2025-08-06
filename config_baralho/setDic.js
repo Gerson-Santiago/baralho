1d962c9af0011bbb8603aad06f9f01ee01fced78
Gerson Santiago <gersondossantossantiago@gmail.com>
2025-08-06 09:10:39 -0300

    Melhorias em setDic.js

--- config_baralho/setDic.js
+++ config_baralho/setDic.js
@@ -1,19 +1,21 @@
 // baralho/config_baralho\dic.js
+import getCartas from "./cartas.js";
+import getNaipes from "./naipes.js";
 
 export default function setDic(deck) {
-    console.log("Iniciando o mapeamento do baralho... {baralho/config_baralho\dic.js}");
-    // Aqui você pode implementar a lógica de mapeamento do baralho
-    let result = []
-    deck.forEach((element, idx) => {
-        let carta = element[0];
-        let naipe = element[1];
-        //console.log(`Carta: ${carta} = ${carta.charCodeAt(0)}, Naipe: ${naipe}`);
-        result.push({ carta, naipe, idx });
-    })
-    // console.log("Mapeamento concluído.");
-    console.log(`Total de cartas mapeadas: ${result.length}`);
-    console.log("Detalhes das cartas mapeadas:", result);
+    const valores = getCartas();
+    const naipes = getNaipes();
+
+    const result = deck.map(carta => {
+        const naipe = carta.slice(-1);
+        const valor = carta.slice(0, -1);
+        const valorIdx = valores.indexOf(valor);
+        const naipeIdx = naipes.indexOf(naipe);
+
+        return { carta, valor, naipe, valorIdx, naipeIdx };
+    });
+
     return result;
-    // Retorna o resultado do mapeamento
 }
-setDic(["3♠", "Q♠", "3♦", "A♣", "A♦", "10♥", "2♣", "K♦"]); // Exemplo de uso
+
+console.log(setDic(["3♠", "Q♠", "3♦", "A♣", "A♦", "10♥", "2♣", "K♦"]));
